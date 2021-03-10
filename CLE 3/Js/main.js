efc06ce2f1471a8ea6f3595451f50f5219f12568
Lucas-Git-Hub <lvanwaas@hotmail.com>
2021-03-10 13:37:16 +0100

    Clickable werkt via console (Geeft code aan), als je op een plaatje klikt can een show. Ook nieuwe variablen toegevoegd samen met functie playingFieldClickHandler().

--- CLE 3/Js/main.js
+++ CLE 3/Js/main.js
@@ -1,20 +1,21 @@
 window.addEventListener('load',init);
 
-let codes = [1,2,3,4]
+let shows = [1,2,3,4];
+let codes = ['5231','213','12313','1232'];
 let currentClickedImage;
 let playField;
 
 //Initialize after the DOM is ready
 function init(){
     playField = document.getElementById('playing-field');
-    playField.addEventListener('click',playingFieldClickHandler);
+    playField.addEventListener('click', playingFieldClickHandler);
 
     createPlayField();
 }
 
 //Make a list of shows
 function createPlayField(){
-    for (let i = 0; i < codes.length; i++){
+    for (let i = 0; i < shows.length; i++){
         //Main Element
         let curtain = document.createElement('div');
         curtain.classList.add('polaroid');
@@ -35,7 +36,24 @@ function createPlayField(){
     }
 }
 
-//Make the pictures of shows clickable
+//Make the pictures of shows clickable and shows code
 function playingFieldClickHandler(e){
+    //Check if its and image otherwise stop function
+    if (e.target.nodeName !== "IMG"){
+        return;
+    }
 
+    //If something already was clicked, set it back to the curtain
+    if (currentClickedImage) {
+        currentClickedImage.setAttribute('src','../img/gordijn.jpg');
+    }
+
+    //Show code if clicked on the image
+    let code = e.target;
+    let codeIndex = code.dataset.index;
+    code.setAttribute('string',codes[codeIndex])
+
+    //Save code
+    currentClickedImage = code;
+    console.log(currentClickedImage);
 }
