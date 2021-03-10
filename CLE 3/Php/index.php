a607d0865617ef75ae08563384bc49e972a2e70b
Lucas-Git-Hub <lvanwaas@hotmail.com>
2021-03-10 14:10:02 +0100

    Menubar met temporary colors. Aparte css file voor het menubar en menubar om tussen account en index te gaan.

--- CLE 3/Php/index.php
+++ CLE 3/Php/index.php
@@ -9,9 +9,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="../Css/style.css"/>
     <title>Codes JustDanceAR</title>
 </head>
+<link rel="stylesheet" type="text/css" href="../Css/menubar.css"/>
+<nav>
+    <ul>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="account.php">Account</a></li>
+    </ul>
+</nav>
+<link rel="stylesheet" type="text/css" href="../Css/style.css"/>
 <body>
 <section>
     <div id="playing-field"></div>
