c644509dd584f417a38d0f7a7483b3ef59d00a7f
Lucas-Git-Hub <lvanwaas@hotmail.com>
2021-03-24 12:43:33 +0100

    Begin Form

--- CLE 3/Php/inlog.php
+++ CLE 3/Php/inlog.php
@@ -1,1 +1,23 @@
 <?php
+
+?>
+
+<!doctype html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inlogpagina</title>
+</head>
+<link rel="stylesheet" type="text/css" href="../Css/menubar.css"/>
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a class="active" href="account.php">Account</a></li>
+    </ul>
+</nav>
+<link rel="stylesheet" type="text/css" href="../Css/style.css"/>
+<body>
+<form></form>
+</body>
