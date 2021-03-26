d9f17a3c83f6fd6572d3e974f34ec6b902b5787e
Jesper Sliedrecht <1006222@hr.nl>
2021-03-26 13:16:45 +0100

    inlogformulier met begin aan cookies

--- CLE 3/Php/inlog.php
+++ CLE 3/Php/inlog.php
@@ -1,5 +1,42 @@
 <?php
+/** @var  $db */
+session_start();
 
+if (isset($_POST['submit'])){
+    require_once 'database.php';
+
+    $email = mysqli_escape_string($db,$_POST['email']);
+    $password = $_POST['password'];
+
+    $errors = [];
+    if ($email == '') {
+        $errors['email'] = "verplicht veld";
+    }
+
+    if ($password == '') {
+        $errors['password'] = "verplicht veld";
+    }
+
+    if (empty($errors)){
+     $query = "SELECT * FROM users WHERE email = '$email'";
+     $result = mysqli_query($db, $query);
+     if (mysqli_num_rows($result) == 1){
+         $user = mysqli_fetch_assoc($result);
+         if (password_verify($password, $user['password'])){
+            $_SESSION['loggedInUser'] = [
+                    'userMail' => $user['email'],
+                'password' => $user['password'],
+
+            ];
+         }
+     }
+    }
+}
+
+if (isset($_SESSION['loggedInUser'])){
+    header('Location: account.php');
+    exit;
+}
 ?>
 
 <!doctype html>
@@ -19,5 +56,19 @@
 </nav>
 <link rel="stylesheet" type="text/css" href="../Css/style.css"/>
 <body>
-<form></form>
+<form id="inlog" action="" method="post" enctype="multipart/form-data">
+    <div>
+        <label for="email">email</label>
+        <input id="email" type="text" name="email" value = "<?= isset($email) ? htmlentities($email) : '' ?>"/>
+        <span class="errors"><?= isset($errors['email']) ? $errors['email'] : '' ?></span>
+    </div>
+    <div>
+        <label for="password">password</label>
+        <input id="password" type="password" name="password" value = "<?= isset($password) ? htmlentities($password) : '' ?>"/>
+        <span class="errors"><?= isset($errors['password']) ? $errors['password'] : '' ?></span>
+    </div>
+    <div>
+        <input id="submit" type="submit" value="login">
+    </div>
+</form>
 </body>
