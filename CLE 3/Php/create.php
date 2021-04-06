885ab2805c86b2776cc5ffc77930611618b5e705
Jesper Sliedrecht <1006222@hr.nl>
2021-04-06 19:39:03 +0200

    create form toegevoegd

--- CLE 3/Php/create.php
+++ CLE 3/Php/create.php
@@ -13,11 +13,18 @@ if (!isset($_SESSION['loggedInUser'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Create</title>
 </head>
 <link rel="stylesheet" type="text/css" href="../Css/style.css">
 <body>
 <link rel="stylesheet" type="text/css" href="../Css/form.css">
+<h1>Voer een nieuwe code in:</h1>
+<form action="" method="post">
+    <label for="code"></label>
+    <input id="code" type="text" name="code" value="<?= isset($code) ? htmlentities($code) : '' ?>" placeholder="Code"/>
+    <!--shows error from errors array-->
+    <span><?= isset($errors['code']) ? $errors['code'] : '' ?></span>">
+</form>
 
 </body>
 </html>
