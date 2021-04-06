22358410570b75f0dd661f672970ceb9eb3deb0f
Jesper Sliedrecht <1006222@hr.nl>
2021-04-06 19:33:26 +0200

    link naar stylesheet en html gelegd

--- CLE 3/Php/create.php
+++ CLE 3/Php/create.php
@@ -4,3 +4,20 @@ if (!isset($_SESSION['loggedInUser'])) {
     header('Location: inlog.php');
     exit;
 }
+
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" type="text/css" href="../Css/style.css">
+<body>
+<link rel="stylesheet" type="text/css" href="../Css/form.css">
+
+</body>
+</html>
